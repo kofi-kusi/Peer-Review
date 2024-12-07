800fa81ff76b6712afb1d6ed9e84023b6776ab06
kusi <appaukusi05@gmail.com>
2024-12-07 17:24:28 +0000

    add: view for index page

--- reviews/views.py
+++ reviews/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 from . import models
 
 # Create your views here.
+def index(request):
+    """The Student Dashboard page."""
+    return render ("reviews/student-dashboard/student-dashboard.html")
